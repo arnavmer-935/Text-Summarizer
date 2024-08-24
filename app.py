46a9c0b3ac57db9906cf1883b2d26266e5e4a773
arnavmer-935 <arniemerani@gmail.com>
2024-08-24 10:13:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,20 +6,35 @@ from main import divide_text_chunks
 
 
 st.set_page_config("Text Summarizer")
-st.header("Chat with Google Gemini Pro LLM Model and get your Summary")
+st.header("TEXT SUMMARIZER")
+st.header("Upload any file/URL or input any text to generate a summary in a language of your choice")
 
-user_input = st.text_area("Please Provide the Complete text...")
-language_options = ['English', 'Hindi', 'Marathi', 'Spanish']
-selected_language = st.selectbox("Select Language:", language_options, index=language_options.index('English'))
-button = st.button("Submit And Get Summary..")
+user_input = st.text_area("Please enter input text here")
+language_options = ["English", "Mandarin", "Spanish", "Hindi", "Bengali", "Portuguese", "Russian", 
+    "Japanese", "German", "French", "Turkish", "Italian", "Korean", "Vietnamese", 
+    "Tamil", "Urdu", "Persian", "Arabic", "Dutch", "Polish", "Thai", "Swedish", 
+    "Czech", "Greek", "Hungarian", "Romanian", "Bulgarian", "Hebrew", "Danish", 
+    "Norwegian", "Finnish", "Malay", "Indonesian", "Tagalog", "Serbian", "Croatian", 
+    "Slovak", "Slovenian", "Lithuanian", "Latvian", "Estonian", "Georgian", "Armenian", 
+    "Azerbaijani", "Kazakh", "Uzbek", "Turkmen", "Mongolian", "Pashto", "Sindhi", 
+    "Punjabi", "Gujarati", "Marathi", "Kannada", "Malayalam", "Telugu", "Sinhala", 
+    "Nepali", "Burmese", "Khmer", "Lao", "Bislama", "Fijian", "Tongan", "Samoan", 
+    "Maori", "Hawaiian", "Yoruba", "Hausa", "Swahili", "Zulu", "Xhosa", "Amharic", 
+    "Tigrinya", "Somali", "Wolof", "Fula", "Igbo", "Shona", "Sesotho", "Tswana", 
+    "Herero", "Afrikaans", "Chewa", "Mandinka", "Javanese", "Sundanese", "Balinese", 
+    "Madurese", "Minangkabau", "Acehnese", "Tetum", "Papiamento", "Quechua", "Aymara", 
+    "Guarani", "Mapudungun", "Nahuatl"]
+    
+selected_language = st.selectbox("Select Language:", language_options, index = language_options.index('English'))
+button = st.button("Submit And Get Summary")
 if user_input and button:
-    response = text_summary(user_input,selected_language)
+    response = text_summary(user_input, selected_language)
     st.write(response)
 
-response =""
+response = ""
 with st.sidebar:
-    st.title("Uploaded a Pdf File...")
-    pdf_docs = st.file_uploader("Upload your files in .pdf format and Click on Submit and process your files")
+    st.title("Uploaded any PDF of upto 200MB:")
+    pdf_docs = st.file_uploader("Upload your files in .pdf format and Click on \"Submit and Process\" to process your files.")
     button = st.button("Submit and Process")
     if(button):
          raw_text = read_pdf_text(pdf_docs)
