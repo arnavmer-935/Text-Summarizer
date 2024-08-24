c598439bd3fc8d7b74996cdc4d95c92496b19745
arnavmer-935 <arniemerani@gmail.com>
2024-08-24 10:07:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,12 @@ import os
 
 
 load_dotenv()
-genai.configure(api_key = GOOGLE_API_KEY)
-llm_model = ChatGoogleGenerativeAI(model = "gemini-pro",temperature=0.4)
+genai.configure(api_key = os.getenv("GOOGLE_API_KEY"))
+
+llm_model = ChatGoogleGenerativeAI(model = "gemini-pro", temperature = 0.4)
 
 def read_pdf_text(pdf_docs):   
-    # provide the path of  pdf file/files.
     pdfreader = PdfReader(pdf_docs)
-    # read text from pdf
     text = ''
     for i, page in enumerate(pdfreader.pages):
         content = page.extract_text()
@@ -27,27 +26,27 @@ def read_pdf_text(pdf_docs):
 
 
 def divide_text_chunks(text):
-    ## Splittting the text
     chunks_prompt="""
     Please summarize the below text:
     text:`{text}'
     Summary:
     """
-    map_prompt_template=PromptTemplate(input_variables=['text'],
-                                        template=chunks_prompt)
+    map_prompt_template=PromptTemplate(input_variables = ['text'],
+                                        template = chunks_prompt)
 
-
-    final_combine_prompt='''
+    final_combine_prompt = '''
     Provide a final summary of the entire text with these important points.
     Add a Generic Motivational Title,
     Start the precise summary with an introduction and provide the
-    summary in few paragraps for the text based on the length of the given text.
-    include the observation,results,conclusions sections also.
-    try to include any important equations or mathematical formulas if you find in the text.
+    summary in few paragraphs for the text, based on the length of the given text.
+    Include important observations, results, conclusions and facts as well.
+    Include any important equations or mathematical formulas you find in the text.
+    Ignore all in-text questions.
+    Also highlight character sketches.
     Speech: `{text}`
     '''
-    final_combine_prompt_template=PromptTemplate(input_variables=['text'],
-                                                template=final_combine_prompt)
+    final_combine_prompt_template = PromptTemplate(input_variables = ['text'],
+                                                template = final_combine_prompt)
 
 
 
@@ -56,31 +55,29 @@ def divide_text_chunks(text):
 
 
     global llm_model
-    chain = load_summarize_chain(llm_model , chain_type='map_reduce' , map_prompt=map_prompt_template,
-    combine_prompt=final_combine_prompt_template,verbose=False)
-
-
+    chain = load_summarize_chain(llm_model, chain_type = 'map_reduce', map_prompt = map_prompt_template,
+    combine_prompt = final_combine_prompt_template, verbose = False)
 
     summary = chain.run(chunks)
     return summary
 
 
-def text_summary(text,language="english"):
-    generic_template='''
+def text_summary(text, language="english"):
+    generic_template = '''
     Write a detailed summary of the following text:
     Context : {text}
     Translate the precise summary to {language}.
     '''
 
-    prompt=PromptTemplate(
-        input_variables=['text','language'],
-        template=generic_template
+    prompt = PromptTemplate(
+        input_variables = ['text','language'],
+        template = generic_template
     )
 
     global llm_model
-    llm_chain=LLMChain(llm=llm_model,prompt=prompt)
+    llm_chain = LLMChain(llm = llm_model, prompt = prompt)
 
-    summary=llm_chain.invoke({'text':text,'language':language})
-    return summary
+    summary = llm_chain.invoke({'text':text,'language':language})
+    return summary["text"]
   
 
